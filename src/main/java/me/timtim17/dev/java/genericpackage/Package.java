3a5f26fceba85486436423b37dccb6fbf2b0ecd6
Austin Jenchi <timtim17@users.noreply.github.com>
2016-06-16 20:07:49 -0700

    Add Javadoc

--- src/main/java/me/timtim17/dev/java/genericpackage/Package.java
+++ src/main/java/me/timtim17/dev/java/genericpackage/Package.java
@@ -1,6 +1,10 @@
 package me.timtim17.dev.java.genericpackage;
 
 /**
+ * A wrapper class for Object that preserves the object's type.
+ * One simple class that's the same for all data types for storing in
+ * collections (i.e. {@code List<Package<?>>}).
+ *
  * @author Austin Jenchi (timtim17)
  */
 public class Package<T> {
